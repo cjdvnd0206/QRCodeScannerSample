2ef2b72ff586a1db0b27cea21e7c53d7e0215ad1
cjdvnd0206 <cjdvndaudnjf@naver.com>
2022-07-03 12:15:33 +0900

    Initial Commit

---
+++ QRCodeScannerSample/QRCodeScannerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeScannerSampleApp.swift
+//  QRCodeScannerSample
+//
+//  Created by 윤병진 on 2022/07/03.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeScannerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
